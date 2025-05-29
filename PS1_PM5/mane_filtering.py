df00ec4ff8b8147f8e45d2369f92c969203b8a59
Lumber_3billion <nw.kim@3billion.io>
2025-05-29 15:45:18 +0900

    .

---
+++ PS1_PM5/mane_filtering.py
@@ -0,0 +1,30 @@
+def mane_filtering():
+    """
+    0.mane_filtering과 동일
+    clinvar_pathogenic_missense_hgvs_docker.txt >> filtered_mane_select_clinvar.tsv
+    """
+
+    file_path = 'clinvar_pathogenic_missense_hgvs_docker.txt'
+    output_path = 'filtered_mane_select_clinvar.tsv'
+
+    with open(file_path, encoding='utf-8') as f_in, open(output_path, 'w', encoding='utf-8') as f_out:
+        found_header = False
+        for line in f_in:
+            if not found_header and line.startswith('#Uploaded_variation'): # header를 아직 찾지 못했고, line 이 header 일 때
+                header = line.lstrip('#')
+                f_out.write(header)
+                header_split = header.strip().split('\t')
+                print (header_split)
+                if 'MANE_SELECT' not in header_split: #test 필요
+                    print ("Please annotate MANE")
+                    return
+                mane_index = header_split.index('MANE_SELECT')
+                found_header = True
+                continue
+            if found_header:
+                variant_data = line.strip().split('\t')
+                if variant_data[mane_index] != '-':
+                    f_out.write(line)
+    print("Complete")
+
+mane_filtering()
