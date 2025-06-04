c2cdd24d03df6c544021c67b0ade07ba0a956d87
Lumber_3billion <nw.kim@3billion.io>
2025-06-04 16:51:51 +0900

    .

--- PS1_PM5/clinvar_mane_filtering.py
+++ PS1_PM5/clinvar_mane_filtering.py
@@ -2,8 +2,10 @@ def mane_filtering():
     """
     'clinvar_pathogenic_missense_hgvs_docker.txt' >> '../filtered_mane_select_clinvar.tsv'
 
-    0.mane_filtering과 동일한 기능을 하지만, output file이 parent directory에 생성된다. (1.main과 같은 directory에 위치해야 함)
-    clinvar_pathogenic_missense_hgvs_docker.txt >> ../filtered_mane_select_clinvar.tsv
+    0_mane_filtering과 동일한 기능을 하지만, output file이 parent directory에 생성된다. (1.main과 같은 directory에 위치해야 함)
+    또한 clinvar_pathogenic_missense_hgvs_docker.txt에 포함된 variant의 경우, 모두가 amino acid 변화를 동반하는 missense variant이기 때문에
+    mane select가 하나도 선택 되지 않은 variant는 존재하지 않을 것이다 (직접 확인을 해보지는 않았지만)
+    따라서 mane select가 하나도 선택 되지 않는 경우는 배제하고, mane_select 된 data만을 filtering하여 ../filtered_mane_select_clinvar.tsv에 저장한다.
     """
 
     file_path = 'clinvar_pathogenic_missense_hgvs_docker.txt'
