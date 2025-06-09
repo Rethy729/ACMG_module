8c688e36449be40e03d0962ab9c9ebac538b0333
Lumber_3billion <nw.kim@3billion.io>
2025-06-09 10:28:03 +0900

    .

--- data_check.py
+++ data_check.py
@@ -4,7 +4,7 @@ def data_check(header):
     :return: 앞으로의 ACMG evidence 판별에 필요한 data가 하나라도 없으면 return False + 어떤 data가 빠져있는지 알려주는 메세지 출력 / 모두 충족하면 return True
     """
 
-    missing_option = [option for option in ['Uploaded_variation', 'Allele', 'Location', 'Consequence', 'gnomADg_AF', 'SIFT', 'PolyPhen','HGVSc', 'HGVSp', 'ClinVar_CLNSIG', 'ClinVar_CLNREVSTAT', ] if option not in header]
+    missing_option = [option for option in ['Uploaded_variation', 'Allele', 'Location', 'Consequence', 'gnomADg_AF', 'SIFT', 'PolyPhen','HGVSc', 'HGVSp', 'ClinVar_CLNSIG', 'ClinVar_CLNREVSTAT'] if option not in header]
     if missing_option:
         print(f"Please annotate {', '.join(missing_option).lower()}")
         return False
