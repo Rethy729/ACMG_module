7cc722d731ce9bd8a4f3aa7f6c7bd7adde6f6655
Lumber_3billion <nw.kim@3billion.io>
2025-06-09 11:02:35 +0900

    .

--- BA1_BS1_PM2/BA1_BS1_PM2.py
+++ BA1_BS1_PM2/BA1_BS1_PM2.py
@@ -7,7 +7,7 @@ def ba1_bs1_pm2(header, variant_data, variant_evidence):
     gnomADg_AF의 값을 ratio로 저장
     ratio >= 0.05 -> BA1 이므로 대응 하는 variant_evidence[11] = True
     0.01 <= ratio < 0.05 -> BS1 이므로 대응 하는 variant_evidence[10] = True
-    ratio <= 0.0001 -> PM2 이므로 대응 하는 variant_evidence[2] = True
+    ratio <= 0.00001 -> PM2 이므로 대응 하는 variant_evidence[2] = True
 
     0.01 (1%) 는 Carrier frequency 기준 (보인자)
     0.00001 (0.001%) 는 Incidence 기준 (유병률) ( (4억/80억) / (5000~7000) ~= 0.00001 )
